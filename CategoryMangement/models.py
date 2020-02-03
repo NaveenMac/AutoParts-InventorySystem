6432f849ef72caa2a00599368373f0ee0d2f7c8c
NAVEEN CHAUHAN <naveen.chauhan@digitalindia.gov.in>
2020-02-03 14:45:11 +0530

    Category Model Created

--- CategoryMangement/models.py
+++ CategoryMangement/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    shortName = models.CharField(max_length=10)
+    
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(,max_length=255)
+    shortName = models.CharField(max_length=10)
