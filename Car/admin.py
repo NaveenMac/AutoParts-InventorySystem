c50d3613b10de401f43bf8223efc8386ce388b4f
NAVEEN CHAUHAN <naveen.chauhan@digitalindia.gov.in>
2020-02-11 17:31:03 +0530

    minor changes

--- Car/admin.py
+++ Car/admin.py
@@ -6,14 +6,22 @@ from Car.models import CarList
 class CarMakerAdmin(admin.ModelAdmin):
     fields = ('name','shortName')
     list_display = ('name','short_name')
-    list_filter = ('name',)
+    
     
     def short_name(self,obj):
         return obj.shortName
         
     
 class CarListAdmin(admin.ModelAdmin):
-    pass
+    fields = ('maker','modelline','year','modifications')
+    list_display = ('maker_list','modelline','year','modifications')
+    search_fields = ['carmaker']
+    def view_car_maker(self,obj):
+        return obj.maker.name
+        
+    def maker_list(self,obj):
+        return obj.maker.name
     
+    view_car_maker.empty_value_display = '???'
 admin.site.register(CarMaker, CarMakerAdmin)
 admin.site.register(CarList, CarListAdmin)
