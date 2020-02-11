1198121c11c4435f21221db0be813154776f58ff
NAVEEN CHAUHAN <naveen.chauhan@digitalindia.gov.in>
2020-02-11 15:53:56 +0530

    modified listing of objects

--- Car/admin.py
+++ Car/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from Car.models import CarMaker
-
 from Car.models import CarList
 # Register your models here.
 
-admin.site.register(CarMaker)
-admin.site.register(CarList)
+class CarMakerAdmin(admin.ModelAdmin):
+    fields = ('name','shortName')
+    list_display = ('name','short_name')
+    list_filter = ('name',)
+    
+    def short_name(self,obj):
+        return obj.shortName
+        
+    
+class CarListAdmin(admin.ModelAdmin):
+    pass
+    
+admin.site.register(CarMaker, CarMakerAdmin)
+admin.site.register(CarList, CarListAdmin)
