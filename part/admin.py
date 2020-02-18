5f516ac1f3cb86cd921ac746040ba575ee31b1c7
NAVEEN CHAUHAN <naveen.chauhan@digitalindia.gov.in>
2020-02-18 18:05:54 +0530

    Part Category added

--- part/admin.py
+++ part/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from part.models import PartCategory
 
+from django import forms
 # Register your models here.
+
+class PartCategoryAdmin(admin.ModelAdmin):
+    fields = ['name','parent','is_class']
+    list_display = ('name','parent','is_class','created_on')
+    
+    def parent_list(self,obj):
+        return obj.parent.name
+      
+    class CustomModelChoiceField(forms.ModelChoiceField):
+        def label_from_instance(self, obj):
+            return obj.name
+        
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'parent':
+            return self.CustomModelChoiceField(queryset = PartCategory.objects)
+
+            return super(PartCategoryAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+            
+admin.site.register(PartCategory, PartCategoryAdmin)
--- part/models.py
+++ part/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+# Create your models here.
+class PartCategory(models.Model):
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey(
+           'self',
+           default=None,
+           null=True,
+           blank=True,
+           on_delete=models.CASCADE
+       )
+    is_class = models.BooleanField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
